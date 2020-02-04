112b665f7fa373b2e032e2feb96341e8a7c42360
Alejandro Chang <alejandrochang10@gmail.com>
2020-02-03 21:14:58 -0800

    first commita

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // state = {
+
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <input name="input1" type="text" placeholder="input"/><br />
+          <input name="input2" type="text" placeholder="input"/><br />
+          <button placeholder="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
